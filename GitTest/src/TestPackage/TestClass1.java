c99e842c5f4cddbd5b063ea1d9f5b78800648b33
zerock <zerock@MSDN-SPECIAL>
2018-11-08 14:25:27 +0900

    처음 해보는 git

---
+++ GitTest/src/TestPackage/TestClass1.java
@@ -0,0 +1,10 @@
+package TestPackage;
+
+public class TestClass1 {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+
+	}
+
+}
---
+++ GitTest/src/TestPackage/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author zerock
+ *
+ */
+package TestPackage;
